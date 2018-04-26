c8051e43dca385eeb0f76592e0f3ffe22c0cb72e
jherrera <juan.herrera@ext.prosegur.com>
2018-04-26 12:58:33 -0300

    feature metodos

--- app/service2.py
+++ app/service2.py
@@ -1,24 +1,40 @@
 import json
 import os
 from flask import Flask, request
-from flask_json import FlaskJSON, JsonError, json_response, as_json
+from flask_json import FlaskJSON, JsonError, json_response
 
 app = Flask(__name__)
 FlaskJSON(app)
 
-'''
 
-
-
-@app.route('/get_value')
-@as_json
-def get_value():
-    return dict(value=12)
-'''
 @app.route('/json/<string:id>', methods=['GET'])
 def get(id):
-    return read(id)
+    try:
+        value = read(id)
+        return str(value)
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='No existe registro')
 
+@app.route('/json/<string:id>', methods=['DELETE'])
+def delete(id):
+    try:
+        remove(id)
+        return "DELETE"
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='No existe registro')
+
+
+@app.route('/json', methods=['PUT'])
+def put():
+    data = request.get_json(force=True)
+    try:
+        id = str(data['id'])
+        remove(id)
+        save(id, data)
+
+        return "UPDATED"
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='No existe registro')
 
 
 @app.route('/json', methods=['POST'])
@@ -43,12 +59,18 @@ def save(id, data):
 
 
 def read(id):
-    file = os.path.join('registro/', id)
-    with open(file) as loadid:
-        getdata =json.load(loadid)
-    return getdata
-def delete(id):
-    return 'ok'
+    try:
+        file = os.path.join('registro/', id)
+        with open(file) as loadid:
+            getdata =json.load(loadid)
+        return getdata
+
+    except (KeyError, TypeError, ValueError):
+        return "Error Desconocido"
+
+def remove(id):
+    os.remove('registro/'+id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
